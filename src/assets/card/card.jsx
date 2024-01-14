b65f67350d20ad82b4b1bbd88c61d988d50681e9
Islomjon Abduganiyev <islomjonabduganiyev@Islomjons-MacBook-Air.local>
2024-01-14 19:12:09 +0500

    cards was made

---
+++ src/assets/card/card.jsx
@@ -0,0 +1,63 @@
+import "./style.css"
+import data from './data.json'
+
+
+export default function Index() {
+  return (
+    <div className="block">
+      {
+        data.map(element => {
+          return (<div className="cardText">
+          <div className="block__card">
+              <div className="name">
+                <h3>Name:</h3>
+                <p>{element.name}</p>
+              </div>
+              <div className="userName">
+                <h3>User name:</h3>
+                <p>{element.username}</p>
+              </div>
+              <div className="email">
+                <h3>Email:</h3>
+                <p>{element.email}</p>
+              </div>
+              <div className="tel">
+                <h3>Phone number:</h3>
+                <p>{element.phone}</p>
+              </div>
+                  <div className="webSite">
+                    <h3>Web site:</h3>
+                    <p>{element.website}</p>
+                  </div>
+
+
+
+                  <div className="name">
+                    <h3>Street:</h3>
+                    <p>{element.address.street}</p>
+                  </div>
+
+                  <div className="name">
+                    <h3>Suite:</h3>
+                    <p>{element.address.suite}</p>
+                  </div>
+
+                  <div className="name">
+                    <h3>City:</h3>
+                    <p>{element.address.city}</p>
+                  </div>
+
+                  <div className="name">
+                    <h3>Company name:</h3>
+                    <p>{element.company.name}</p>
+                  </div>
+
+
+          </div>
+        </div>)
+        
+        })
+      }
+    </div>
+  )
+}
